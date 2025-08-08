6adf7bae858bf21877a380a640594a321a99ad44
mayur patil <mrpatil27@gmail.com>
2025-08-08 10:35:36 +0000

    Initial commit with Terraform, Node.js app, and Jenkins pipeline

---
+++ terraform-eks/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.2"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.21.0"
+
+  cluster_name    = "nodejs-eks-cluster"
+  cluster_version = "1.30"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      max_size     = 2
+      min_size     = 1
+      instance_types = ["t3.small"]
+    }
+  }
+}
